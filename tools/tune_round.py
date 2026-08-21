7f47cdd8dfb4f5b70150fdf7ef2750f7230387ca
svy04 <105825627+svy04@users.noreply.github.com>
2026-08-21 20:19:24 +0900

    직전 블록 문맥 실험 되돌림, 실측 기록

--- tools/tune_round.py
+++ tools/tune_round.py
@@ -5,7 +5,7 @@
 """
 import subprocess, sys, os, re
 
-CANDS = [0, 1, 2]
+CANDS = [0, 1]
 SRC = ("src/transform.c src/color.c src/predict.c src/bitio.c "
        "src/rangecoder.c src/encode.c src/decode.c tools/cli.c")
 
