d10df059e349788d1a61f8c1e100c4110439401c
svy04 <105825627+svy04@users.noreply.github.com>
2026-08-22 05:52:59 +0900

    왜곡 자에 블록 경계 가중 추가

--- tools/tune_round.py
+++ tools/tune_round.py
@@ -5,7 +5,7 @@
 """
 import subprocess, sys, os, re
 
-CANDS = [5, 6]
+CANDS = [16, 32, 64]
 SRC = ("src/transform.c src/color.c src/predict.c src/bitio.c "
        "src/rangecoder.c src/encode.c src/decode.c tools/cli.c")
 
